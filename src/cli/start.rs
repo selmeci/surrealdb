d0c675613ad85654d911fc4dc3192154b5f3d5e6
Roman Selmeci <selmeci.roman@gmail.com>
2023-05-25 17:24:46 +0200

    feat(cli/start): add support for AWS Lambda configuration

--- src/cli/start.rs
+++ src/cli/start.rs
@@ -1,4 +1,6 @@
 use super::config;
+#[cfg(feature = "aws-lambda")]
+use crate::cli::config::LambdaConfig;
 use crate::cnf::LOGO;
 use crate::dbs;
 use crate::env;
@@ -7,14 +9,29 @@ use crate::iam;
 use crate::net;
 use futures::Future;
 
-pub fn init(matches: &clap::ArgMatches) -> Result<(), Error> {
+#[cfg(feature = "aws-lambda")]
+type InitMatches = LambdaConfig;
+#[cfg(not(feature = "aws-lambda"))]
+type InitMatches = clap::ArgMatches;
+
+#[cfg(not(feature = "aws-lambda"))]
+pub fn init(matches: &InitMatches) -> Result<(), Error> {
 	with_enough_stack(init_impl(matches))
 }
 
-async fn init_impl(matches: &clap::ArgMatches) -> Result<(), Error> {
+#[cfg(feature = "aws-lambda")]
+pub async fn init(matches: &InitMatches) -> Result<(), Error> {
+	init_impl(matches).await
+}
+
+async fn init_impl(matches: &InitMatches) -> Result<(), Error> {
 	// Initialize opentelemetry and logging
+	#[cfg(not(feature = "aws-lambda"))]
 	crate::o11y::builder().with_log_level(matches.get_one::<String>("log").unwrap()).init();
+	#[cfg(feature = "aws-lambda")]
+	crate::o11y::builder().with_log_level(&matches.log).init();
 	// Check if a banner should be outputted
+	#[cfg(not(feature = "aws-lambda"))]
 	if !matches.is_present("no-banner") {
 		// Output SurrealDB logo
 		println!("{LOGO}");
@@ -28,6 +45,7 @@ async fn init_impl(matches: &clap::ArgMatches) -> Result<(), Error> {
 	// Start the kvs server
 	dbs::init().await?;
 	// Start the web server
+	#[cfg(not(feature = "aws-lambda"))]
 	net::init().await?;
 	// All ok
 	Ok(())
